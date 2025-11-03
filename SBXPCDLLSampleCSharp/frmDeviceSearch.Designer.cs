068333a49c6cca02f8fb8ed09f8a431dc1d79188
Krishna Kumar <krishnakumarbooks@gmail.com>
2025-11-03 16:30:52 +0530

    Add project files.

---
+++ SBXPCDLLSampleCSharp/frmDeviceSearch.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SBXPCDLLSampleCSharp
+{
+    partial class frmDeviceSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ProductName";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(121, 13);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 1;
+            this.txtProductName.Text = "Sx00-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Search Duration(ms)";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(121, 39);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(100, 20);
+            this.txtDuration.TabIndex = 1;
+            this.txtDuration.Text = "2000";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(248, 30);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(111, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(15, 74);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(344, 446);
+            this.txtResult.TabIndex = 3;
+            // 
+            // frmDeviceSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 531);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDeviceSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "frmDeviceSearch";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmDeviceSearch_Closed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
---
+++ SBXPCDLLSampleCSharp/frmPrtCode.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SBXPCDLLSampleCSharp
+{
+    public partial class frmPrtCode : Form
+    {
+        public frmPrtCode()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGetSerialNumber_Click(object sender, EventArgs e)
+        {
+            string vSerialNumber = "";
+            Boolean vRet;
+            int vErrorCode = 0;
+
+            txtSerialNo.Text = "";
+            lblMessage.Text = "Waiting...";
+            Application.DoEvents();
+
+            vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+            if (!vRet)
+            {
+                lblMessage.Text = util.gstrNoDevice;
+                return;
+            }
+
+            vRet = sbxpc.SBXPCDLL.GetSerialNumber(Program.gMachineNumber, out vSerialNumber);
+            if (vRet)
+            {
+                txtSerialNo.Text = vSerialNumber;
+                lblMessage.Text = "Success";
+            }
+            else
+            {
+                sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+                lblMessage.Text = util.ErrorPrint(vErrorCode);
+            }
+
+            sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+        }
+
+        private void cmdGetBackupNumber_Click(object sender, EventArgs e)
+        {
+            int vBackupNumber;
+            int vErrorCode = 0;
+            Boolean vRet;
+
+            lblMessage.Text = "Waiting...";
+            txtBackupNo.Text = "";
+            Application.DoEvents();
+
+            vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+            if (!vRet)
+            {
+                lblMessage.Text = util.gstrNoDevice;
+                return;
+            }
+
+            vBackupNumber = sbxpc.SBXPCDLL.GetBackupNumber(Program.gMachineNumber);
+            if (vBackupNumber != 0)
+            {
+                txtBackupNo.Text = Convert.ToString(vBackupNumber);
+                lblMessage.Text = "Success";
+            }
+            else
+            {
+                sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+                lblMessage.Text = util.ErrorPrint(vErrorCode);
+            }
+
+            sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+        }
+
+        private void cmdGetProductCode_Click(object sender, EventArgs e)
+        {
+            string vProductCode = "";
+            Boolean vRet;
+            int vErrorCode = 0;
+
+            txtProductCode.Text = "";
+            lblMessage.Text = "Waiting...";
+            Application.DoEvents();
+
+            vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+            if (!vRet)
+            {
+                lblMessage.Text = util.gstrNoDevice;
+                return;
+            }
+
+            vRet = sbxpc.SBXPCDLL.GetProductCode(Program.gMachineNumber, out vProductCode);
+            if (vRet)
+            {
+                txtProductCode.Text = vProductCode;
+                lblMessage.Text = "Success";
+            }
+            else
+            {
+                sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+                lblMessage.Text = util.ErrorPrint(vErrorCode);
+            }
+
+            sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            Close();
+            Application.OpenForms["frmMain"].Visible = true;
+        }
+
+        private void frmPrtCode_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.OpenForms["frmMain"].Visible = true;
+        }
+
+        private void frmPrtCode_Load(object sender, EventArgs e)
+        {
+        }
+
+		private void btnGetDeviceUniqueID_Click(object sender, EventArgs e)
+		{
+			string vDeviceUniqueID = "";
+			Boolean vRet;
+			int vErrorCode = 0;
+
+			txtDeviceUniqueID.Text = "";
+			lblMessage.Text = "Waiting...";
+			Application.DoEvents();
+
+			vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+			if (!vRet)
+			{
+				lblMessage.Text = util.gstrNoDevice;
+				return;
+			}
+
+			vRet = sbxpc.SBXPCDLL.GetDeviceUniqueID(Program.gMachineNumber, out vDeviceUniqueID);
+			if (vRet)
+			{
+				txtDeviceUniqueID.Text = vDeviceUniqueID;
+				lblMessage.Text = "Success";
+			}
+			else
+			{
+				sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+				lblMessage.Text = util.ErrorPrint(vErrorCode);
+			}
+
+			sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+		}
+
+		private void btnGetSoftwareKey_Click(object sender, EventArgs e)
+		{
+			string vSoftwareKey = "";
+			Boolean vRet;
+			int vErrorCode = 0;
+
+			txtSoftwareKey.Text = "";
+			lblMessage.Text = "Waiting...";
+			Application.DoEvents();
+
+			vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+			if (!vRet)
+			{
+				lblMessage.Text = util.gstrNoDevice;
+				return;
+			}
+
+			string strXML = null;
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "REQUEST", "GetDeviceInfoExt");
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "MSGTYPE", "request");
+			sbxpc.SBXPCDLL.XML_AddLong(ref strXML, "MachineID", Program.gMachineNumber);
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "ParamName", "softwareKey");
+
+			vRet = sbxpc.SBXPCDLL.GeneralOperationXML(Program.gMachineNumber, ref strXML);
+
+			if (vRet)
+			{
+				if(sbxpc.SBXPCDLL.XML_ParseString(ref strXML, "Value1", out vSoftwareKey))
+					txtSoftwareKey.Text = vSoftwareKey;
+				lblMessage.Text = "Success";
+			}
+			else
+			{
+				sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+				lblMessage.Text = util.ErrorPrint(vErrorCode);
+			}
+
+			sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+		}
+
+		private void btnSetSoftwareKey_Click(object sender, EventArgs e)
+		{
+			string vSoftwareKey = txtSoftwareKey.Text.Trim();
+			Boolean vRet;
+			int vErrorCode = 0;
+
+			lblMessage.Text = "Waiting...";
+			Application.DoEvents();
+
+			vRet = sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0); // 0 : false
+			if (!vRet)
+			{
+				lblMessage.Text = util.gstrNoDevice;
+				return;
+			}
+
+			string strXML = null;
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "REQUEST", "SetDeviceInfoExt");
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "MSGTYPE", "request");
+			sbxpc.SBXPCDLL.XML_AddLong(ref strXML, "MachineID", Program.gMachineNumber);
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "ParamName", "softwareKey");
+			sbxpc.SBXPCDLL.XML_AddString(ref strXML, "Value1", vSoftwareKey);
+
+			vRet = sbxpc.SBXPCDLL.GeneralOperationXML(Program.gMachineNumber, ref strXML);
+
+			if (vRet)
+			{
+				lblMessage.Text = "Success";
+			}
+			else
+			{
+				sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+				lblMessage.Text = util.ErrorPrint(vErrorCode);
+			}
+
+			sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+		}
+
+        private void btnGetDevDetailInfo_Click(object sender, EventArgs e)
+        {
+            Boolean vRet = true;
+            int vErrorCode = 0;
+
+            lblMessage.Text = "Waiting...";
+            Application.DoEvents();
+
+            if (!sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 0)) // 0 : false
+            {
+                lblMessage.Text = util.gstrNoDevice;
+                return;
+            }
+
+            string strXML = null;
+            sbxpc.SBXPCDLL.XML_AddString(ref strXML, "REQUEST", "GetDeviceDetails");
+            sbxpc.SBXPCDLL.XML_AddString(ref strXML, "MSGTYPE", "request");
+            sbxpc.SBXPCDLL.XML_AddLong(ref strXML, "MachineID", Program.gMachineNumber);
+
+            vRet = sbxpc.SBXPCDLL.GeneralOperationXML(Program.gMachineNumber, ref strXML);
+
+            if (vRet)
+            {
+                txtDeviceDetailInfo.Text = strXML;
+
+                string sMacAddr;
+                sbxpc.SBXPCDLL.XML_ParseString(ref strXML, "MAC", out sMacAddr);
+                lblMessage.Text = "Success! MAC Addr = " + sMacAddr;
+            }
+            else
+            {
+                sbxpc.SBXPCDLL.GetLastError(Program.gMachineNumber, out vErrorCode);
+                lblMessage.Text = util.ErrorPrint(vErrorCode);
+            }
+
+            sbxpc.SBXPCDLL.EnableDevice(Program.gMachineNumber, 1); // 1 : true
+        }
+    }
+}
